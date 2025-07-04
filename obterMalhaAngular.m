37bb8e5c6a8b4eaf00c7cb5c4964c728abcb6871
danilo-mir <danilo.mir271@gmail.com>
2025-07-04 19:15:05 -0300

    fix: malha angular

--- obterMalhaAngular.m
+++ obterMalhaAngular.m
@@ -1,11 +1,25 @@
-function dinamica = obterMalhaAngular(controlador, planta)
+function [dinamica, controlador] = obterMalhaAngular()
 
-Kp = controlador.Kp;
-Kv = controlador.Kv;
+planta = obterPlanta();
+requisitos = obterRequisitos();
+
+tr = requisitos.theta.tr;
+Mp = requisitos.theta.Mp;
+
+J = planta.J;
+
+xi = -log(Mp)/sqrt(pi^2+(log(Mp))^2);
+wn = (pi-acos(xi))/(sqrt(1-xi^2)*tr);
+
+Kv = 2*xi*wn*J;
+Kp = wn/(2*xi);
 
 J = planta.J;
 
 s = tf('s');
 dinamica = (Kp*Kv)/(J*s^2+Kv*s+Kp*Kv);
+
+controlador.Kv = Kv;
+controlador.Kp = Kp;
 
 end
